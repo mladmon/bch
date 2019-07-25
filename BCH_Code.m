e7078d72baf08c177ad83413adddd9f953d65d52
Mario Admon <mladmon589@gmail.com>
2019-07-24 18:03:56 -0700

    Fixed formatting

--- BCH_Code.m
+++ BCH_Code.m
@@ -27,9 +27,8 @@ g_x = de2bi(36783, 'left-msb');
 % Create 16-bit binary represenation of "A"
 info = de2bi(65, k, 'left-msb');
 
-% Calculate checkbits
-% Append a number of zeros equal to the degree of g(x) to info. This will
-% be our dividend.
+% Calculate checkbits. Append a number of zeros equal to the degree of g(x) 
+% to info. This will be our dividend.
 dividend = info;
 dividend(end+1:end+length(g_x)-1) = 0;
 
@@ -105,93 +104,93 @@ twomu_lmu(1) = -1;
 
 % Fill in the table.
 for j = 2:length(mu)
-    if d_mu(j) == 0
-        % Calculate sigma_x(j+1).
-        sigma_x(:,:,j+1) = sigma_x(:,:,j);
-    else
-        % Find a preceding row (row rho) with the most positive twomu_lmu
-        % and d_mu ~= 0.
-        rho = 0;
-        most_pos = -10000;
-        for k = 1:(j-1)
-            if(twomu_lmu(k) > most_pos && d_mu(k) ~= -1)
-                most_post = twomu_lmu(k);
-                rho = k;
-            end
-        end
+	if d_mu(j) == 0
+		% Calculate sigma_x(j+1).
+		sigma_x(:,:,j+1) = sigma_x(:,:,j);
+	else
+		% Find a preceding row (row rho) with the most positive twomu_lmu
+		% and d_mu ~= 0.
+		rho = 0;
+		most_pos = -10000;
+		for k = 1:(j-1)
+			if(twomu_lmu(k) > most_pos && d_mu(k) ~= -1)
+				most_post = twomu_lmu(k);
+				rho = k;
+			end
+		end
+
+		% Calculate sigma_x(j+1).
+		% Step 1: Calculate power of x^2(mu-rho)
+		power = 2*(mu(j)-mu(rho));
+
+		% Step 2: Calculate x^(power)*(sigma_x(rho))
+		sigma_rho = sigma_x(:, :, rho);
+		for i = 1:power
+			sigma_rho(1) = sigma_rho(2);
+			sigma_rho(2) = sigma_rho(3);
+			sigma_rho(3) = sigma_rho(4);
+			sigma_rho(4) = sigma_rho(5);
+			sigma_rho(5) = -1;
+		end
+
+		% Step 3: Calculate d_mu*d_rho^(-1).
+		coeff = p_mult(d_mu(j),p_inv(d_mu(rho)));
         
-        % Calculate sigma_x(j+1).
-        % Step 1: Calculate power of x^2(mu-rho)
-        power = 2*(mu(j)-mu(rho));
+		% Step 4: Calculate Step3 * Step2
+		for i = 1:length(sigma_rho)
+			if sigma_rho(i) ~= -1
+				sigma_rho(i) = p_mult(sigma_rho(i),coeff);
+			end
+		end
         
-        % Step 2: Calculate x^(power)*(sigma_x(rho))
-        sigma_rho = sigma_x(:, :, rho);
-        for i = 1:power
-            sigma_rho(1) = sigma_rho(2);
-            sigma_rho(2) = sigma_rho(3);
-            sigma_rho(3) = sigma_rho(4);
-            sigma_rho(4) = sigma_rho(5);
-            sigma_rho(5) = -1;
-        end
-        
-        % Step 3: Calculate d_mu*d_rho^(-1).
-        coeff = p_mult(d_mu(j),p_inv(d_mu(rho)));
-        
-        % Step 4: Calculate Step3 * Step2
-        for i = 1:length(sigma_rho)
-            if sigma_rho(i) ~= -1
-                sigma_rho(i) = p_mult(sigma_rho(i),coeff);
-            end
-        end
-        
-        % Step 5: Calculate Sigma_x(:,:,j) + Step4
-        sigma_x(:,:,j+1) = poly_add(GF, sigma_x(:,:,j),sigma_rho);
-        
-    end
+		% Step 5: Calculate Sigma_x(:,:,j) + Step4
+		sigma_x(:,:,j+1) = poly_add(GF, sigma_x(:,:,j),sigma_rho);
+
+	end
+
+	% If mu == t-1, terminate the algorithm.
+	if mu(j) == 2
+		break; 
+	end    
+
+	% Calculate l_mu(j+1).
+	degree = 0;
+	for i = 1:length(sigma_x(:,:,j+1))
+		if sigma_x(:,i,j+1) ~= -1
+			degree = length(sigma_x(:,:,j+1)) - i;
+			break;
+		end
+	end
+
+	l_mu(j+1) = degree;     
+
+	% Calculate d_mu(j+1).
+	L = l_mu(j+1);
+	S_sigma_coeff = -1.*ones(1,2,3);
+	for i = 0:L
+		S_sub = 2*mu(j) + (3-i);
+		sigma_term = length(sigma_x(:,:,j+1)) - i;
+		S_sigma_coeff(:,:,i+1) = [S_sub, sigma_term];
+	end
+
+	alpha_poly = -1.*ones(1,3);
+	for i = 1:length(S_sigma_coeff)
+		if S_sigma_coeff(1,1,i) ~= -1
+			alpha_poly(i) = p_mult(S_a(S_sigma_coeff(1,1,i)), sigma_x(1,S_sigma_coeff(1,2,i),j+1));
+		end                                                              
+	end
     
-    % If mu == t-1, terminate the algorithm.
-    if mu(j) == 2
-       break; 
-    end    
-    
-    % Calculate l_mu(j+1).
-    degree = 0;
-    for i = 1:length(sigma_x(:,:,j+1))
-        if sigma_x(:,i,j+1) ~= -1
-            degree = length(sigma_x(:,:,j+1)) - i;
-            break;
-        end
-    end
-    
-    l_mu(j+1) = degree;     
-        
-    % Calculate d_mu(j+1).
-    L = l_mu(j+1);
-    S_sigma_coeff = -1.*ones(1,2,3);
-    for i = 0:L
-        S_sub = 2*mu(j) + (3-i);
-        sigma_term = length(sigma_x(:,:,j+1)) - i;
-        S_sigma_coeff(:,:,i+1) = [S_sub, sigma_term];
-    end
-    
-    alpha_poly = -1.*ones(1,3);
-    for i = 1:length(S_sigma_coeff)
-        if S_sigma_coeff(1,1,i) ~= -1
-            alpha_poly(i) = p_mult(S_a(S_sigma_coeff(1,1,i)), sigma_x(1,S_sigma_coeff(1,2,i),j+1));
-        end                                                              
-    end
-    
-    binary_alpha_poly = zeros(1,500);
-    for i = 1:length(alpha_poly)
-       if alpha_poly(i) ~= -1
-           binary_alpha_poly(length(binary_alpha_poly) - alpha_poly(i)) = 1;
-       end
-    end
-    
-    d_mu(j+1) = poly2power(GF, polynomial_mod(binary_alpha_poly,prim_poly));
-                                                                            
-    % Calculate twomu_lmu(j+1)
-    twomu_lmu(j+1) = 2*mu(j+1) - l_mu(j+1);
+	binary_alpha_poly = zeros(1,500);
+	for i = 1:length(alpha_poly)
+		if alpha_poly(i) ~= -1
+			binary_alpha_poly(length(binary_alpha_poly) - alpha_poly(i)) = 1;
+		end
+	end
+
+	d_mu(j+1) = poly2power(GF, polynomial_mod(binary_alpha_poly,prim_poly));
+
+	% Calculate twomu_lmu(j+1)
+	twomu_lmu(j+1) = 2*mu(j+1) - l_mu(j+1);
 end
 
 % Error locator polynomial is the last entry of sigma_x
@@ -205,57 +204,57 @@ error_loc_poly = sigma_x(:,:,length(sigma_x));
 roots = -1.*ones(1,3);
 count = 1;
 for i = 0:length(GF)-2
-    error_loc_poly_a = zeros(1, 200);
-    size = length(error_loc_poly_a);
-    for j = 1:length(error_loc_poly)
-        if error_loc_poly(j) ~= -1
-            bin_one = p_mult(error_loc_poly(j),i*(length(error_loc_poly)-j));
-            error_loc_poly_a(size - bin_one) = 1;
-        end
-    end
-    if polynomial_mod(error_loc_poly_a, prim_poly) == 0
-        roots(count) = i;
-        count = count+1;
-    end
+	error_loc_poly_a = zeros(1, 200);
+	size = length(error_loc_poly_a);
+	for j = 1:length(error_loc_poly)
+		if error_loc_poly(j) ~= -1
+			bin_one = p_mult(error_loc_poly(j),i*(length(error_loc_poly)-j));
+			error_loc_poly_a(size - bin_one) = 1;
+		end
+	end
+	if polynomial_mod(error_loc_poly_a, prim_poly) == 0
+		roots(count) = i;
+		count = count+1;
+	end
 end
 
 % Find inverse of roots
 inv_roots = -1.*ones(1,3);
 for i = 1:length(roots)
-    if roots(i) ~= -1
-        % Find n-Tuple Form of the root in GF
-        root_i = GF(roots(i)+2, :);
-        
-        % Convert n-Tuple Form to Polynomial Form
-        root_i_poly = nTuple2Poly(root_i);
-       
-        % Search through GF for root_i_poly's inverse.
-        for k = 2:length(GF)
-           poly = poly_mult(root_i_poly, nTuple2Poly(GF(k,:)));
-           binary_poly = zeros(1,10);
-           size = length(binary_poly);
-           for j = 1:length(poly)
-               if poly(j) ~= -1
-                   binary_poly(size - poly(j)) = 1;
-               end
-           end  
-           
-           % If the mod of binary_poly and prim_poly is 1, binary_poly is
-           % the multiplicative inverse.
-           mod = polynomial_mod(binary_poly, prim_poly);
-           inverse = 1;
-           for x = 1:length(mod)
-               if mod(x) == 1 && x ~= length(mod)
-                   inverse = 0;
-               end
-           end
-           
-           if inverse
-               inv_roots(i) = k - 2;
-               break;
-           end
-        end
-    end
+	if roots(i) ~= -1
+		% Find n-Tuple Form of the root in GF
+		root_i = GF(roots(i)+2, :);
+
+		% Convert n-Tuple Form to Polynomial Form
+		root_i_poly = nTuple2Poly(root_i);
+
+		% Search through GF for root_i_poly's inverse.
+		for k = 2:length(GF)
+			poly = poly_mult(root_i_poly, nTuple2Poly(GF(k,:)));
+			binary_poly = zeros(1,10);
+			size = length(binary_poly);
+			for j = 1:length(poly)
+				if poly(j) ~= -1
+					binary_poly(size - poly(j)) = 1;
+				end
+			end
+
+			% If the mod of binary_poly and prim_poly is 1, binary_poly is
+			% the multiplicative inverse.
+			mod = polynomial_mod(binary_poly, prim_poly);
+			inverse = 1;
+			for x = 1:length(mod)
+				if mod(x) == 1 && x ~= length(mod)
+					inverse = 0;
+				end
+			end
+
+			if inverse
+				inv_roots(i) = k - 2;
+				break;
+			end
+		end
+	end
 end
 
 % Error polynomial: gives location of the errors.
@@ -264,9 +263,9 @@ e_x = inv_roots;
 % XOR the received codeword and the error polynomial to fix errors!!!
 binary_e_x = zeros(1,31);
 for i = 1:length(e_x)
-    if e_x(i) ~= -1
-        binary_e_x(length(binary_e_x)- e_x(i)) = 1;
-    end
+	if e_x(i) ~= -1
+		binary_e_x(length(binary_e_x)- e_x(i)) = 1;
+	end
 end
 
 fx_codeword = xor(rx_codeword, binary_e_x);
